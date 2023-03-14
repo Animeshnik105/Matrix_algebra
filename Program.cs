f68cb94adb8687e15e51beff7bed44ced845bac7
gr691_ssa <gr691_ssa@110-9>
2023-03-14 15:02:50 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Вы открыли приложение <<Матричная алгебра>>\n1 - сложение\n2 - вычитание\n3 - умножение на число\n4 - транспонирование матрицы\n5 - умножение на транспонированную матрицу\n6 - перестановка двух строк матрицы\n7 - перестановка матрицы согласно вектору\n8 - обратная матрица\n");
+while (0 == 0)
+{
+	Console.Write("\nВыберите действие нажав соответствующую цифру: ");
+	int action = Convert.ToInt32(Console.ReadLine());
+	if (action == 1)
+	{
+		Console.WriteLine("Я хочу питсы");
+	}
+	if (action == 2)
+	{
+
+	}
+	if (action == 3)
+	{
+
+	}
+	if (action == 4)
+	{
+
+	}
+	if (action == 5)
+	{
+
+	}
+	if (action == 6)
+	{
+
+	}
+	if (action == 7)
+	{
+
+	}
+	if (action == 8)
+	{
+
+	}
+}
+
